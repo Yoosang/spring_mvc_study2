a8a21d11fbf52bec594491e2a9e9c9c70c24fd04
usang-window <usang6325@gmail.com>
2022-07-20 17:33:21 +0900

    체크박스-단일

---
+++ src/main/java/hello/thymeleaf/itemservice/domain/item/DeliveryCode.java
@@ -0,0 +1,13 @@
+package hello.thymeleaf.itemservice.domain.item;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class DeliveryCode {
+
+    private String code;
+    private String displayName;
+
+}
