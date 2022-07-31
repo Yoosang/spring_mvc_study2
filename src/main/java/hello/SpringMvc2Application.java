db5fde8172e04f053adce2a48042c11929224abf
usang <usang6325@gmail.com>
2022-07-31 16:54:51 +0900

    스프링 메세지 소스 사용

--- src/main/java/hello/SpringMvc2Application.java
+++ src/main/java/hello/SpringMvc2Application.java
@@ -2,6 +2,9 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
 
 @SpringBootApplication
 public class SpringMvc2Application {
@@ -10,4 +13,14 @@ public class SpringMvc2Application {
 		SpringApplication.run(SpringMvc2Application.class, args);
 	}
 
+	/**
+	 * Spring boot는 해당 코드 없이 자동등록
+	 */
+//	@Bean
+//	public MessageSource messageSource() {
+//		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+//		messageSource.setBasenames("messages", "errors");
+//		messageSource.setDefaultEncoding("utf-8");
+//		return messageSource;
+//	}
 }
