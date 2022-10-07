a4dc601d5574002741d7b4ec3fd71f1c8063662b
usang-window <usang6325@gmail.com>
2022-10-07 16:36:25 +0900

    로그인처리 HttpSession 사용

--- src/main/java/hello/itemservice/web/login/HomeController.java
+++ src/main/java/hello/itemservice/web/login/HomeController.java
@@ -11,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -57,7 +58,7 @@ public class HomeController {
         return "login/loginHome";
     }
 
-    @GetMapping("/home")
+    //@GetMapping("/home")
     public String homeLoginV3(HttpServletRequest request, Model model){
 
         log.info("homeLoginV3");
@@ -67,6 +68,20 @@ public class HomeController {
         }
 
         Member loginMember = (Member) session.getAttribute(SessionConst.LOGIN_MEMBER);
+        if(loginMember == null){
+            return "login/home";
+        }
+
+        model.addAttribute("member", loginMember);
+        return "login/loginHome";
+    }
+
+    @GetMapping("/home")
+    public String homeLoginV3Spring(@SessionAttribute(name=SessionConst.LOGIN_MEMBER, required = false) Member loginMember
+            , Model model){
+
+        log.info("homeLoginV3Spring");
+
         if(loginMember == null){
             return "login/home";
         }
